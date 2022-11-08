0a8fe56798de9d75a9722cbcaec7d9ac3ab89d81
Kaiser <ahmedkaiser.001@gmail.com>
2022-11-08 18:14:20 +0600

    added database

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fixmo2v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() => {
+
+}
+
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send("Mr. Chef server running")
